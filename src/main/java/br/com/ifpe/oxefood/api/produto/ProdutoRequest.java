727c2448339102228da53a4f1326b5021792d188
aluno <aluno@ifpe>
2023-09-15 17:24:06 -0300

    Aulas 10 e 11

---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -0,0 +1,36 @@
+package br.com.ifpe.oxefood.api.produto;
+
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoRequest {
+
+    private String titulo;
+
+    private int codigoProduto;
+
+    private String descricao;
+
+    private int valorUnitario;
+
+    private double tempMin;
+
+    public Produto build() {
+        return Produto.builder()
+                .titulo(titulo)
+                .codigoProduto(codigoProduto)
+                .descricao(descricao)
+                .valorUnitario(valorUnitario)
+                .tempMin(tempMin)
+                .build();
+
+    }
+
+}

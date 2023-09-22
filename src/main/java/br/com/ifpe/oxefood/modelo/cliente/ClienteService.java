c694abac18d7a35a4df29e494094f1c695d7c785
aluno <aluno@ifpe>
2023-09-22 17:19:51 -0300

    Aulas 12 e 13

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,11 +1,14 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+//import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 
 @Service
 public class ClienteService {
@@ -21,5 +24,14 @@ public class ClienteService {
     return repository.save(cliente);
 
     }
-    
+    public List<Cliente> findAll() {
+  
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+
+        return repository.findById(id).get();
+    }
+
 }
